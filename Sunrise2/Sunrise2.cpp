7b474582508f4fea568c6c4c6f67e8557bb53087
Codie Stella <git@codie.gg>
2023-01-15 13:26:07 +0000

    Added h3 delta cache_release support

--- Sunrise2/Sunrise2.cpp
+++ Sunrise2/Sunrise2.cpp
@@ -35,6 +35,7 @@ BOOL bIgnoreTrueskill = FALSE;
 
 DWORD Halo3_Pimps_LSP_Addr = 0x821A7768;
 DWORD Halo3_Delta_Mar9_cache_test_LSP_Addr = 0x82343270;
+DWORD Halo3_Delta_Mar9_cache_release_LSP_Addr = 0x82446F90;
 DWORD Halo3_Beta_May1_LSP_Addr = 0x820DE740;
 DWORD Halo3_Beta_May15_LSP_Addr = 0x820DE738;
 DWORD Halo3_Epsilon_LSP_Addr = 0x8248E820;
@@ -160,6 +161,15 @@ VOID Initialise()
 					Sunrise_Dbg("[Sunrise2] Halo 3 Delta (cache_test, Mar 9th) detected! Initialising hooks...\n");
 					SetupNetDllHooks();
 					SetupLSPHook(Halo3_Delta_Mar9_cache_test_LSP_Addr);
+
+					XNotify(L"Halo Sunrise Intialised!");
+					break;
+				}
+				case 0x45F1026C: // Halo 3 Delta cache_release
+				{
+					Sunrise_Dbg("[Sunrise2] Halo 3 Delta (cache_release, Mar 9th) detected! Initialising hooks...\n");
+					SetupNetDllHooks();
+					SetupLSPHook(Halo3_Delta_Mar9_cache_release_LSP_Addr);
 
 					XNotify(L"Halo Sunrise Intialised!");
 					break;
